fb01983216fdf49f22761d7eff74a3c292514e42
AislynnEdmiston <davidandaislynn@gmail.com>
2020-03-07 15:04:41 -0500

    added my project router

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const helmet = require("helmet")
+const projectRouter = require("./projects/projects-router")
 
 const server = express()
 const port = process.env.PORT || 4000
@@ -7,10 +8,18 @@ const port = process.env.PORT || 4000
 server.use(helmet())
 server.use(express.json())
 
+server.use("/projects", projectRouter)
+
 server.use((err, req, res, next) => {
     console.log(err)
     res.status(500).json({
         message: "Something went wrong"
+    })
+})
+
+server.get("/", (req, res, next) => {
+    res.json({
+        message: "Welcome to the sprint!",
     })
 })
 
